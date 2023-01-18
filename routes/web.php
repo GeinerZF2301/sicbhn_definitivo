74fa916956757390f9a03b6df66a80350fdb2c49
Alfi1591 <alfieri.mora.jimenez@est.una.ac.cr>
2023-01-17 22:38:33 -0600

    se pego la plantilla admin, quedo funcional, es de  iniciar a editar

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', function () {
+    return view('layouts.admin');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
