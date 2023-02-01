4723185eb52fc3edde66687bd18878b07e9f0b36
kriscardenas26 <krisscardenas260103@gmail.com>
2023-01-31 23:12:28 -0600

    Se hicieron cambios en todas las vistas de la parte cliente, se implemento un layout nuevo que contiene parte del navbar y el footer para ahorrar lineas de codigo, junto con la directiva yield se implemento en c/u de las vistas

---
+++ resources/views/principal/donaciones/donaciones-index.blade.php
@@ -0,0 +1,229 @@
+
+@extends('layouts.navbar')
+@section('content')
+    <!-- Navbar Start -->
+    <nav
+      class="navbar navbar-expand-lg bg-white navbar-light sticky-top py-lg-0 px-4 px-lg-5 wow fadeIn"
+      data-wow-delay="0.1s"
+    >
+ 
+      <a href="index.html" class="navbar-brand p-0">
+        <img class="img-fluid me-3" src="zoofari/img/LogoCorredor.png" alt="Icon" />
+        <h1 class="m-0 text-primary">SICBHN</h1>
+      </a>
+      <button
+        type="button"
+        class="navbar-toggler"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse py-4 py-lg-0" id="navbarCollapse">
+        <div class="navbar-nav ms-auto">
+          <a href="{{ url('/') }}" class="nav-item nav-link">Inicio</a>
+          <a href="{{ url('/voluntariado') }}" class="nav-item nav-link">Voluntariados</a>
+          <a href="{{ url('/campanna') }}" class="nav-item nav-link">Campañas</a>
+          <a href="{{ url('/donaciones') }}" class="nav-item nav-link active">Donaciones</a>
+          <div class="nav-item dropdown">
+            <a href="#"
+              class="nav-link dropdown-toggle"
+              data-bs-toggle="dropdown"
+              >Sobre Nosotros</a>
+              
+            <div class="dropdown-menu rounded-0 rounded-bottom m-0">
+              <a href="{{ url('/sobrenosotros') }}" class="dropdown-item">Sobre nosotros</a>
+              <a href="{{ url('/galeria') }}" class="dropdown-item">Galería</a>
+              <a href="{{ url('/horavisita') }}" class="dropdown-item">Horas de visita</a>
+              <a href="{{ url('/documentos') }}" class="dropdown-item">Documentos</a>
+            </div>
+          </div>
+          <a href="{{ url('/contactos') }}" class="nav-item nav-link">Contactos</a>
+        </div>
+        <a href="{{ route('login') }}" class="btn btn-warning"
+          >Iniciar Sesión<i class="fa fa-right ms-0"></i
+        ></a>
+        <a href="{{ route('register') }}" class="btn btn-success"
+          >Registrarse<i class="fa fa-right ms-0"></i
+        ></a>
+      </div>
+    </nav>
+   <!-- Page Header Start -->
+   <div
+      class="container-fluid header-bg py-5 mb-5 wow fadeIn"
+      data-wow-delay="0.1s">
+      <div class="container py-5">
+        <h1 class="display-4 text-white mb-3 animated slideInDown">Donaciones</h1>
+        <nav aria-label="breadcrumb animated slideInDown">
+          <ol class="breadcrumb mb-0">
+            <li class="breadcrumb-item">
+              <a class="text-white" href="{{ url('/') }}">Inicio</a>
+            </li>
+            <li class="breadcrumb-item text-primary active" aria-current="page">
+              Donaciones
+            </li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+    <!-- Page Header End -->
+
+    <!-- About Start -->
+    <div class="container-xxl py-5">
+      <div class="container">
+        <div class="row g-5">
+          <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+            <p><span class="text-primary me-2">#</span>Welcome To Zoofari</p>
+            <h1 class="display-5 mb-4">
+              Why You Should Visit
+              <span class="text-primary">Zoofari</span> Park!
+            </h1>
+            <p class="mb-4">
+              Stet no et lorem dolor et diam, amet duo ut dolore vero eos. No
+              stet est diam rebum amet diam ipsum. Clita clita labore, dolor duo
+              nonumy clita sit at, sed sit sanctus dolor eos.
+            </p>
+            <h5 class="mb-3">
+              <i class="far fa-check-circle text-primary me-3"></i>Free Car
+              Parking
+            </h5>
+            <h5 class="mb-3">
+              <i class="far fa-check-circle text-primary me-3"></i>Natural
+              Environment
+            </h5>
+            <h5 class="mb-3">
+              <i class="far fa-check-circle text-primary me-3"></i>Professional
+              Guide & Security
+            </h5>
+            <h5 class="mb-3">
+              <i class="far fa-check-circle text-primary me-3"></i>World Best
+              Animals
+            </h5>
+            <a class="btn btn-primary py-3 px-5 mt-3" href="">Read More</a>
+          </div>
+          <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+            <div class="img-border">
+              <img class="img-fluid" src="zoofari/img/about.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- About End -->
+
+    <!-- Facts Start -->
+    <div
+      class="container-xxl bg-primary facts my-5 py-5 wow fadeInUp"
+      data-wow-delay="0.1s"
+    >
+      <div class="container py-5">
+        <div class="row g-4">
+          <div
+            class="col-md-6 col-lg-3 text-center wow fadeIn"
+            data-wow-delay="0.1s"
+          >
+            <i class="fa fa-paw fa-3x text-primary mb-3"></i>
+            <h1 class="text-white mb-2" data-toggle="counter-up">12345</h1>
+            <p class="text-white mb-0">Total Animal</p>
+          </div>
+          <div
+            class="col-md-6 col-lg-3 text-center wow fadeIn"
+            data-wow-delay="0.3s"
+          >
+            <i class="fa fa-users fa-3x text-primary mb-3"></i>
+            <h1 class="text-white mb-2" data-toggle="counter-up">12345</h1>
+            <p class="text-white mb-0">Daily Vigitors</p>
+          </div>
+          <div
+            class="col-md-6 col-lg-3 text-center wow fadeIn"
+            data-wow-delay="0.5s"
+          >
+            <i class="fa fa-certificate fa-3x text-primary mb-3"></i>
+            <h1 class="text-white mb-2" data-toggle="counter-up">12345</h1>
+            <p class="text-white mb-0">Total Membership</p>
+          </div>
+          <div
+            class="col-md-6 col-lg-3 text-center wow fadeIn"
+            data-wow-delay="0.7s"
+          >
+            <i class="fa fa-shield-alt fa-3x text-primary mb-3"></i>
+            <h1 class="text-white mb-2" data-toggle="counter-up">12345</h1>
+            <p class="text-white mb-0">Save Wild Life</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Facts End -->
+
+    <!-- Testimonial Start -->
+    <div class="container-xxl py-5">
+      <div class="container">
+        <h1
+          class="display-5 text-center mb-5 wow fadeInUp"
+          data-wow-delay="0.1s"
+        >
+          Our Clients Say!
+        </h1>
+        <div
+          class="owl-carousel testimonial-carousel wow fadeInUp"
+          data-wow-delay="0.1s"
+        >
+          <div class="testimonial-item text-center">
+            <img
+              class="img-fluid rounded-circle border border-2 p-2 mx-auto mb-4"
+              src="zoofari/img/testimonial-1.jpg"
+              style="width: 100px; height: 100px"
+            />
+            <div class="testimonial-text rounded text-center p-4">
+              <p>
+                Clita clita tempor justo dolor ipsum amet kasd amet duo justo
+                duo duo labore sed sed. Magna ut diam sit et amet stet eos sed
+                clita erat magna elitr erat sit sit erat at rebum justo sea
+                clita.
+              </p>
+              <h5 class="mb-1">Patient Name</h5>
+              <span class="fst-italic">Profession</span>
+            </div>
+          </div>
+          <div class="testimonial-item text-center">
+            <img
+              class="img-fluid rounded-circle border border-2 p-2 mx-auto mb-4"
+              src="img/testimonial-2.jpg"
+              style="width: 100px; height: 100px"
+            />
+            <div class="testimonial-text rounded text-center p-4">
+              <p>
+                Clita clita tempor justo dolor ipsum amet kasd amet duo justo
+                duo duo labore sed sed. Magna ut diam sit et amet stet eos sed
+                clita erat magna elitr erat sit sit erat at rebum justo sea
+                clita.
+              </p>
+              <h5 class="mb-1">Patient Name</h5>
+              <span class="fst-italic">Profession</span>
+            </div>
+          </div>
+          <div class="testimonial-item text-center">
+            <img
+              class="img-fluid rounded-circle border border-2 p-2 mx-auto mb-4"
+              src="zoofari/img/testimonial-3.jpg"
+              style="width: 100px; height: 100px"
+            />
+            <div class="testimonial-text rounded text-center p-4">
+              <p>
+                Clita clita tempor justo dolor ipsum amet kasd amet duo justo
+                duo duo labore sed sed. Magna ut diam sit et amet stet eos sed
+                clita erat magna elitr erat sit sit erat at rebum justo sea
+                clita.
+              </p>
+              <h5 class="mb-1">Patient Name</h5>
+              <span class="fst-italic">Profession</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Testimonial End -->
+    @endsection
+    <!-- Navbar End -->
+
+
+    
--- resources/views/principal/sobrenosotros/sobrenosotros-index.blade.php
+++ resources/views/principal/sobrenosotros/sobrenosotros-index.blade.php
@@ -1,82 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>SICBHN</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-    <meta content="" name="keywords" />
-    <meta content="" name="description" />
 
-    <!-- Favicon -->
-    <link href="zoofari/img/LogoCorredor.png" rel="icon" />
-
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Quicksand:wght@600;700&display=swap"
-      rel="stylesheet"
-    />
-
-    <!-- Icon Font Stylesheet -->
-    <link
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"
-      rel="stylesheet"
-    />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
-      rel="stylesheet"
-    />
-
-    <!-- Libraries Stylesheet -->
-    <link href="{{asset('zoofari/lib/animate/animate.min.css')}}" rel="stylesheet" />
-    <link href="{{asset('zoofari/lib/lightbox/css/lightbox.min.css')}}" rel="stylesheet" />
-    <link href="{{asset('zoofari/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet" />
-
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{asset('zoofari/css/bootstrap.min.css')}}" rel="stylesheet" />
-
-    <!-- Template Stylesheet -->
-    <link href="{{asset('zoofari/css/style.css')}}" rel="stylesheet" />
-  </head>
-
-<body>
-
-
-    <!-- Topbar Start -->
-    <div class="container-fluid bg-light p-0 wow fadeIn" data-wow-delay="0.1s">
-      <div class="row gx-0 d-none d-lg-flex">
-        <div class="col-lg-7 px-5 text-start">
-          <div class="h-100 d-inline-flex align-items-center py-3 me-4">
-            <small class="fa fa-map-marker-alt text-primary me-2"></small>
-            <small>Guanacaste, Costa Rica</small>
-          </div>
-          <div class="h-100 d-inline-flex align-items-center py-3">
-            <small class="far fa-clock text-primary me-2"></small>
-            <small>Lunes a Viernes: 08:00 AM - 12:00 PM</small>
-          </div>
-        </div>
-        <div class="col-lg-5 px-5 text-end">
-          <div class="h-100 d-inline-flex align-items-center py-3 me-4">
-            <small class="fa fa-phone-alt text-primary me-2"></small>
-            <small>+(506) 8670-3535</small>
-          </div>
-          <div class="h-100 d-inline-flex align-items-center">
-            <a class="btn btn-sm-square bg-white text-primary me-1" href=""
-              ><i class="fab fa-facebook-f"></i
-            ></a>
-            <a class="btn btn-sm-square bg-white text-primary me-1" href=""
-              ><i class="fab fa-instagram"></i
-            ></a>
-            <a class="btn btn-sm-square bg-white text-primary me-0" href=""
-              ><i class="fab fa-youtube"></i
-            ></a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Topbar End -->
-
+@extends('layouts.navbar')
+@section('content')
     <!-- Navbar Start -->
     <nav
       class="navbar navbar-expand-lg bg-white navbar-light sticky-top py-lg-0 px-4 px-lg-5 wow fadeIn"
@@ -96,7 +20,7 @@
       </button>
       <div class="collapse navbar-collapse py-4 py-lg-0" id="navbarCollapse">
         <div class="navbar-nav ms-auto">
-          <a href="{{ url('/') }}" class="nav-item nav-link active">Inicio</a>
+          <a href="{{ url('/') }}" class="nav-item nav-link">Inicio</a>
           <a href="{{ url('/voluntariado') }}" class="nav-item nav-link">Voluntariados</a>
           <a href="{{ url('/campanna') }}" class="nav-item nav-link">Campañas</a>
           <a href="{{ url('/donaciones') }}" class="nav-item nav-link">Donaciones</a>
@@ -107,7 +31,7 @@
               >Sobre Nosotros</a
             >
             <div class="dropdown-menu rounded-0 rounded-bottom m-0">
-              <a href="{{ url('/sobrenosotros') }}" class="dropdown-item">Sobre nosotros</a>
+              <a href="{{ url('/sobrenosotros') }}" class="dropdown-item active">Sobre nosotros</a>
               <a href="{{ url('/galeria') }}" class="dropdown-item">Galería</a>
               <a href="{{ url('/horavisita') }}" class="dropdown-item">Horas de visita</a>
               <a href="{{ url('/documentos') }}" class="dropdown-item">Documentos</a>
@@ -317,110 +241,4 @@
         </div>
     </div>
     <!-- Funciones End -->
-
-
-    
-    <!-- Footer Start -->
-    <div
-      class="container-fluid footer bg-dark text-light footer mt-5 pt-5 wow fadeIn"
-      data-wow-delay="0.1s"
-    >
-      <div class="container py-5">
-        <div class="row g-5">
-          <div class="col-lg-3 col-md-6">
-            <h5 class="text-light mb-4">Más información</h5>
-            <p class="mb-2">
-              <i class="fa fa-map-marker-alt me-3"></i>Guanacaste, Costa Rica
-            </p>
-            <p class="mb-2">
-              <i class="fa fa-phone-alt me-3"></i>+(506) 8670-3535
-            </p>
-            <p class="mb-2">
-              <i class="fa fa-envelope me-3"></i>info.cbhn@gmail.com
-            </p>
-            <div class="d-flex pt-2">
-              <a class="btn btn-outline-light btn-social" href=""
-                ><i class="fab fa-facebook-f"></i
-              ></a>
-              <a class="btn btn-outline-light btn-social" href=""
-              ><i class="fab fa-instagram"></i
-            ></a>
-              <a class="btn btn-outline-light btn-social" href=""
-                ><i class="fab fa-youtube"></i
-              ></a>
-
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6">
-            <h5 class="text-light mb-4">Enlaces rápidos</h5>
-            <a class="btn btn-link" href="{{ url('/sobrenosotros') }}">Sobre Nosotros</a>
-            <a class="btn btn-link" href="{{ url('/contactos') }}">Contactos</a>
-            <a class="btn btn-link" href="{{ url('/horavisita') }}">Horas de Visita</a>
-            <a class="btn btn-link" href="{{ url('/documentos') }}">Documentos</a>
-          </div>
-          <div class="col-lg-3 col-md-6">
-            <h5 class="text-light mb-4 ">Enlaces populares</h5>
-            <a class="btn btn-link" href="{{ url('/galeria') }}">Galería</a>
-            <a class="btn btn-link" href="{{ url('/voluntariado') }}">Voluntariados</a>
-            <a class="btn btn-link" href="{{ url('/campanna') }}">Campañas</a>
-            <a class="btn btn-link" href="{{ url('/donaciones') }}">Donaciones</a>
-          </div>
-          <div class="col-lg-3 col-md-6">
-            <h5 class="text-light mb-4 ">El Corredor Biológico te da la bienvenida</h5>
-            <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-            <div class="position-relative mx-auto" style="max-width: 400px">
-              <input
-                class="form-control border-0 w-100 py-3 ps-4 pe-5"
-                type="text"
-                placeholder="Your email"
-              />
-              <button
-                type="button"
-                class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2"
-              >
-                SignUp
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="container">
-        <div class="copyright">
-          <div class="row">
-            <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-              &copy; <a class="border-bottom" href="#">SICBHN</a>, Todos los derechos reservados.
-            </div>
-            <div class="col-md-6 text-center text-md-end">
-              <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-              Diseñado por:
-              <a class="border-bottom" href="https://htmlcodex.com"
-                >HTML Codex</a
-              >
-              <br />Distribuido por:
-              <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Footer End -->
-
-    <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"
-      ><i class="bi bi-arrow-up"></i
-    ></a>
-
-    <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{asset('zoofari/lib/wow/wow.min.js')}}"></script>
-    <script src="{{asset('zoofari/lib/easing/easing.min.js')}}"></script>
-    <script src="{{asset('zoofari/lib/waypoints/waypoints.min.js')}}"></script>
-    <script src="{{asset('zoofari/lib/counterup/counterup.min.js')}}"></script>
-    <script src="{{asset('zoofari/lib/owlcarousel/owl.carousel.min.js')}}"></script>
-    <script src="{{asset('zoofari/lib/lightbox/js/lightbox.min.js')}}"></script>
-
-    <!-- Template Javascript -->
-    <script src="{{asset('zoofari/js/main.js')}}"></script>
-</body>
-</html>
+    @endsection
