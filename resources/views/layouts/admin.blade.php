0d3e59208ae5817f852c48822f7439c34c6d8b16
Alfi1591 <alfieri.mora.jimenez@est.una.ac.cr>
2023-03-02 21:08:14 -0600

    Funcionalidad de sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -312,6 +312,10 @@
         <script src="{{ asset('spica/vendors/js/vendor.bundle.base.js') }}"></script>
         <script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.13.2/js/dataTables.bootstrap5.min.js"></script>
+        <script src="{{asset('spica/js/off-canvas.js')}}"></script>
+        <script src="{{asset('spica/js/hoverable-collapse.js')}}"></script>
+        <script src="{{asset('spica/js/template.js')}}"></script>
+        
       </div>
 </body>
 
