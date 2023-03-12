531f039af816cc49bebd8ba39e2d6eb58568c5cc
Alfi1591 <alfieri.mora.jimenez@est.una.ac.cr>
2023-03-11 19:48:59 -0600

    pruebas

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,7 @@ Route::middleware(LocaleCookieMiddleware::class)->group(function () {
 
 
 Auth::routes(['verify' => true]);
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 Route::group(['middleware' => ['auth']], function() {
