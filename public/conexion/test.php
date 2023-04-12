dff1939c6e0cba30caeef4ecb667af5a46cf6563
Alfi1591 <alfieri.mora.jimenez@est.una.ac.cr>
2023-04-11 23:29:36 -0600

    PDF Personas  funcional

--- public/conexion/test.php
+++ public/conexion/test.php
@@ -28,4 +28,14 @@ require('../../public/conexion/conexion.php');
 	$data = $result->fetchall(PDO::FETCH_ASSOC);
 	
 	var_dump($data);
+
+	require('../../public/conexion/conexion.php');
+	$data=new ConexionPersonas();
+	$conexion=$data->conect();
+	$strquery ="SELECT * FROM `personas`";
+	$result = $conexion->prepare($strquery);
+	$result->execute();
+	$data = $result->fetchall(PDO::FETCH_ASSOC);
+	
+	var_dump($data);
 ?>
